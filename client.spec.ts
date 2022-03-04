dcfdf2b6f6f8c10e32387b843ccec28492f876f3
Martin Hoang <martin.hoang@unicorn.com>
2022-03-04 19:09:54 +0100

    implementation of client with tests done

--- client.spec.ts
+++ client.spec.ts
@@ -1,3 +1,146 @@
-it("should actually test the client", () => {
-  throw new Error("but it doesn't.");
+import {SuperDuperRememberer} from "./super-duper-rememberer";
+import {rememberData} from "./client";
+
+jest.setTimeout(99999999);
+let rememberer: SuperDuperRememberer;
+
+beforeEach(() => {
+  rememberer = new SuperDuperRememberer();
 });
+
+it("should successfully remember basic string", async () => {
+  const successfulData = ["basicstringtoremember"];
+  await rememberData(rememberer, successfulData);
+
+  expect(rememberer.memory).toEqual(successfulData);
+});
+
+it("should successfully remember multiple strings", async () => {
+  const successfulData = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
+  await rememberData(rememberer, successfulData);
+
+  expect(rememberer.memory).toEqual(successfulData);
+});
+
+it("should successfully remember multiple strings exceeding one call", async () => {
+  const successfulData = [
+    "ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddda",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddb",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddc",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
+    "ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddde"
+  ];
+  await rememberData(rememberer, successfulData);
+
+  expect(rememberer.memory).toEqual(successfulData);
+});
+
+it("should successfully remember long long long array of strings", async () => {
+  const successfulData = [
+    "ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddda",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddb",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddc",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
+    "ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddde",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddf",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddg",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddh",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddi",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddj",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddk",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddl",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddm",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddn",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddo",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddp",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddq",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddr",
+    "ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddds",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddt",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddu",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddv",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddx",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddy",
+    "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddz"
+  ];
+  await rememberData(rememberer, successfulData);
+
+  expect(rememberer.memory).toEqual(successfulData);
+});
+
+it("should successfully remember 100 strings", async () => {
+  const chars = "abcdefghijklmnopqrstuvwxyz";
+  const charLength = chars.length;
+
+  const successfulData: string[] = [];
+  for (let i = 0; i < 100; i++) {
+    let result = "";
+    for (let j = 0; j < 3; j++) {
+      result += chars.charAt(Math.floor(Math.random() * charLength));
+    }
+    successfulData.push(result);
+  }
+  await rememberData(rememberer, successfulData);
+
+  expect(rememberer.memory).toEqual(successfulData);
+});
+
+it("should throw error DataIsEmptyArray", async () => {
+  const badData: string[] = [];
+
+  expect.assertions(2);
+  try {
+    await rememberData(rememberer, badData);
+  } catch (e: unknown) {
+    expect((e as Error).message).toEqual("Data is empty array, nothing to remember.");
+    expect((e as Error).name).toEqual("DataIsEmptyArray");
+  }
+});
+
+it("should throw error WordIsNotInLowercase", async () => {
+  const badData = [
+    "aaa", "bbb", "cCc", "ddd", "eee"
+  ];
+
+  expect.assertions(2);
+  try {
+    await rememberData(rememberer, badData);
+  } catch (e: unknown) {
+    expect((e as Error).message).toEqual(`Can't process word ${badData[2]}, the word is not lowercase.`);
+    expect((e as Error).name).toEqual("WordIsNotInLowercase");
+  }
+});
+
+it("should throw error WordIsNotInLowercase with numbers", async () => {
+  const badData = [
+    "aaa", "bbb", "123", "ddd", "eee"
+  ];
+
+  expect.assertions(2);
+  try {
+    await rememberData(rememberer, badData);
+  } catch (e: unknown) {
+    expect((e as Error).message).toEqual(`Can't process word ${badData[2]}, the word is not lowercase.`);
+    expect((e as Error).name).toEqual("WordIsNotInLowercase");
+  }
+});
+
+it("should throw error WordExceededMaxLength", async () => {
+  const badData = [
+    "a",
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    "a"
+  ];
+
+  expect.assertions(2);
+  try {
+    await rememberData(rememberer, badData);
+  } catch (e: unknown) {
+    expect((e as Error).message).toEqual(`Can't process word ${badData[1]}, the word is too long to process (${badData[1].length}) (max length of 1 word is ${250}).`);
+    expect((e as Error).name).toEqual("WordExceededMaxLength");
+  }
+});
+
+
--- client.ts
+++ client.ts
@@ -1,8 +1,74 @@
-import { SuperDuperRememberer } from "./super-duper-rememberer";
+import { SuperDuperRememberer, ExecuteStatementCommand, DescribeStatementCommand } from "./super-duper-rememberer";
+
+const delayForRetryCall = Number(process.env.delayForRetryCall) || 1000;
 
 export async function rememberData(
   rememberer: SuperDuperRememberer,
   data: string[]
 ): Promise<void> {
-  // HERE BE DRAGONS
+  const beginStatement = "CAN YOU PLEASE REMEMBER THESE ITEMS?";
+  const endStatement = "K THX BYE.";
+  const statementLength = beginStatement.length + endStatement.length;
+  // beginStatement + endStatement (46) + '' to wrap word + __ 2 white spaces is 50, so string must be max 250 char long
+  const maxLength = 300 - (statementLength + 2 + 2);
+
+  if (data.length === 0) {
+    const e = new Error("Data is empty array, nothing to remember.");
+    e.name = "DataIsEmptyArray";
+    throw e;
+  }
+  data.forEach(d => {
+    if (!/^([a-z]+$)/g.test(d)) {
+      const e = new Error(`Can't process word ${d}, the word is not lowercase.`);
+      e.name = "WordIsNotInLowercase";
+      throw e;
+    }
+    if (d.length > maxLength) {
+      const e = new Error(`Can't process word ${d}, the word is too long to process (${d.length}) (max length of 1 word is ${maxLength}).`);
+      e.name = "WordExceededMaxLength";
+      throw e;
+    }
+  });
+
+  const statements: string[] = [];
+  let stringToRemember = "";
+  for (const str of data) {
+    if (stringToRemember.length === 0) {
+      stringToRemember = `'${str}'`;
+    } else {
+      if (stringToRemember.length + str.length + 3 < maxLength) {
+        stringToRemember += `,'${str}'`;
+      } else {
+        statements.push(stringToRemember);
+        stringToRemember = `'${str}'`;
+      }
+    }
+  }
+  statements.push(stringToRemember);
+
+  for (const statement of statements) {
+    const statementCall = `${beginStatement} ${statement} ${endStatement}`;
+    await executeWaitingCall(rememberer, statementCall);
+  }
 }
+
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+async function executeWaitingCall(rememberer: SuperDuperRememberer,statement: string): Promise<void> {
+  const result = await rememberer.executeCommand(new ExecuteStatementCommand({ statement }));
+  let status = await rememberer.executeCommand(new DescribeStatementCommand( { id: result.id } ));
+
+  while (status.status === "RUNNING") {
+    await delay(delayForRetryCall);
+    status = await rememberer.executeCommand(new DescribeStatementCommand( { id: result.id } ));
+  }
+  if (status.status === "FAILED") {
+    const e = new Error(`SDR end up with error status ${status.status} and message ${status.error}.`);
+    e.name = "SdrEndUpWithFailedStatus";
+    throw e;
+  }
+  if (status.status === "DONE") {
+    return;
+  }
+}
+
